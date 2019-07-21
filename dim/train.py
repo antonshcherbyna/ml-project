8201e7a53b274623085f410020fa109f38cdfad7
Anton Shcherbyna <31895978+antonshcherbyna@users.noreply.github.com>
2019-07-21 22:25:06 +0300

    fix issue with the size of the inputs

--- dim/train.py
+++ dim/train.py
@@ -39,8 +39,8 @@ args = parser.parse_args()
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-transform = transforms.Compose(
-    [transforms.Resize((224, 224)),
+transform = transforms.Compose([
+     #[transforms.Resize((224, 224)),
      transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
